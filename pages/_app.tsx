3ef01beec344150c565645d7fda644d84d985e79
Odynn Aguilar <admin@kaoz.space>
2020-11-19 11:59:09 -0600

    Enable typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import '../styles.css';
+
+// @ts-ignore
 function App({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
 }
 
